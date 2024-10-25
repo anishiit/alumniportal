51caa72b1e58535bd085774b7901f6d96508a834
Anish Kumar Singh <anishsingh74850@gmail.com>
2024-10-25 10:48:42 +0530

    "Updated registration page layout: changed padding values and commented out SVG container"

--- src/app/registration/page.jsx
+++ src/app/registration/page.jsx
@@ -78,9 +78,9 @@ function Page() {
   return (
     <div>
     <section>
-      <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+      <div className="flex items-center justify-center px-4 py-5 sm:px-6 sm:py-16 lg:px-8 lg:py-10">
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-          <div className="mb-2 flex justify-center">
+          {/* <div className="mb-2 flex justify-center">
             <svg
               width="50"
               height="56"
@@ -93,7 +93,7 @@ function Page() {
                 fill="black"
               />
             </svg>
-          </div>
+          </div> */}
           <h2 className="text-center text-2xl font-bold leading-tight text-black">
             Sign in to your account
           </h2>
