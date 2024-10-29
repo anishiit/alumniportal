00433eb84399157b0a6ad8b56100ce5b7e4f57e6
abhaykantmishra <abhaykantmishra004@gmail.com>
2024-10-30 00:00:13 +0530

    small ui changes on profile page

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -250,7 +250,7 @@ export default function ProfileDisplay({ user }) {
             <>
             {
               currentUser?.role === "admin" ? (
-                <Button onClick={() => router.push('/admin')} variant="outline" size="sm" className="mr-2 text-xs sm:text-sm">
+                <Button onClick={() => router.push('/admin')} variant="ghost" size="sm" className="mr-2 text-xs bg-amber-400 hover:bg-amber-400/80 sm:text-sm">
                   Admin
                 </Button>
               ) : (
