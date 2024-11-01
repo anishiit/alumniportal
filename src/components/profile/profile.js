e980f687ccd1e65fa8657de413ca8c0113de8c22
Anish Kumar Singh <anishsingh74850@gmail.com>
2024-11-01 21:32:32 +0530

    "Updated ProfileDisplay component: changed fragment to div, updated button class"

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -231,10 +231,10 @@ export default function ProfileDisplay({ user }) {
                 </Link>
               </div>
               ) : (
-                <>
+                <div className="mt-2 flex items-center justify-center sm:justify-start"><Users className="w-5 h-5 mr-2" />
                 <p className="text-sm text-white mt-1">
                 {usr.connectedUsers?.length} connections </p>
-                </>
+                </div>
               )
             }
             
@@ -287,7 +287,7 @@ export default function ProfileDisplay({ user }) {
             }
             </>
            ):(<Button 
-                 size="lg"  className="text-xs sm:text-sm bg-gradient-to-r from-blue-600 to-indigo-600 text-white flex ">
+                 size="lg"  className="text-xs sm:text-sm bg-blue-600 hover:bg-blue-600/80 text-white flex ">
               {usr.connectedUsers?.includes(String(iscurrent._id)) ? (
                 <Link href='/chat'  className="flex">
                   <MessageCircle className="w-3 h-3 sm:w-4 sm:h-4 mr-1 sm:mr-2 " />
