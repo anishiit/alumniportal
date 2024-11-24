d3984d9cf4174162b31317f79e59203a8195c0ce
Anish Kumar Singh <anishsingh74850@gmail.com>
2024-11-25 02:30:03 +0530

    "Updated sitemap.xml route with hardcoded URLs and lastmod dates"

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -5,37 +5,38 @@ export async function GET() {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>${baseUrl}/</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-      </url>
-      <url>
-        <loc>${baseUrl}/about</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.8</priority>
-      </url>
-      <url>
-        <loc>${baseUrl}/contact</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.9</priority>
-      </url>
-      <url>
-        <loc>${baseUrl}/login</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.7</priority>
-      </url>
-      <url>
-        <loc>${baseUrl}/collegeRegistration</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.6</priority>
-      </url>
-    </urlset>`;
+  <url>
+    <loc>https://www.linklum.in/</loc>
+    <lastmod>2024-11-24T20:56:44.775Z</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.linklum.in/about</loc>
+    <lastmod>2024-11-24T20:56:44.775Z</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.linklum.in/contact</loc>
+    <lastmod>2024-11-24T20:56:44.775Z</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.linklum.in/login</loc>
+    <lastmod>2024-11-24T20:56:44.775Z</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.linklum.in/collegeRegistration</loc>
+    <lastmod>2024-11-24T20:56:44.775Z</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.6</priority>
+  </url>
+</urlset>
+`;
 
   console.log("Sitemap route accessed"); // Debugging log
 
